b9928e3ffe1b8b303601ce3e05141fe3ab7fad96
Oscar Catari Gutierrez <alu0100825893@ull.edu.es>
2016-05-16 17:13:38 +0100

    karma

--- tests/tests.js
+++ tests/tests.js
@@ -1,19 +1,19 @@
 
-let expect = chai.expect;
+let expect = chai.expect
 
 //var PEG = require("./../models/pl0.js");
 
 describe('PL0', function(){
     it ('"=" se puede evaluar como una expresion', function (){
-  
+
        //var obj = PEG.parse("a = 9");
        //var arbol = util.inspect(obj, {depth: null});
        //console.log(obj);
        var x = {n: "Sea",c: "Water"};
         //console.log(cal("a = 6"));
-        
-        
+
+
         expect("aa").to.equal("aa");
     });
-    
+
 });
